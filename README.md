a19aa483f8527459211b4c3e9cd534ad46697ff1
Edward Paget <ed.paget@gmail.com>
2014-01-20 13:41:50 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# nginx-zoo cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx-zoo'
+maintainer       'Edward Paget'
+maintainer_email 'ed@zooniverse.org'
+license          'Apache Public License v2; see LICENSE'
+description      'Installs/Configures nginx-zoo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depens 'nginx', '~> 2.2.0' 
